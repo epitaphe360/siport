92b5742a392acae6d465e640174021218bfe7da6
Siport-sketch <albert28890@gmail.com>
2025-09-03 05:05:27 -0700

    Updated FeaturedExhibitors.tsx

--- src/components/home/FeaturedExhibitors.tsx
+++ src/components/home/FeaturedExhibitors.tsx
@@ -82,7 +82,6 @@ export const FeaturedExhibitors: React.FC = () => {
           </motion.div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
           {featuredExhibitors.map((exhibitor, index) => (
             <motion.div
