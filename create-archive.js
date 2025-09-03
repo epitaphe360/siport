984574d0d25754df8cf81f28b3457dca5c702386
Siport-sketch <albert28890@gmail.com>
2025-09-02 17:08:00 -0700

    Fix ES module syntax in create-archive.js

--- create-archive.js
+++ create-archive.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 // Créer un script simple pour télécharger les fichiers
 const downloadScript = `
