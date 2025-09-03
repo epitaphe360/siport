147a703f4a45a89d83d7420212ea774d466345d2
Siport-sketch <albert28890@gmail.com>
2025-09-03 04:41:11 -0700

    Supprimer le champ de recherche du header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -119,18 +119,6 @@ export const Header: React.FC = () => {
 
           {/* Right side */}
           <div className="flex items-center space-x-4">
-            {/* Search */}
-            <div className="hidden md:block">
-              <div className="relative">
-                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
-                <input
-                  type="text"
-                  placeholder="Rechercher..."
-                  className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                />
-              </div>
-            </div>
-
             {/* Language Selector */}
             <div className="relative">
               <button 
@@ -395,17 +383,6 @@ export const Header: React.FC = () => {
               ))}
             </div>
             
-            {/* Mobile Search */}
-            <div className="mt-4 px-3">
-              <div className="relative">
-                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
-                <input
-                  type="text"
-                  placeholder="Rechercher..."
-                  className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                />
-              </div>
-            </div>
           </div>
         )}
       </div>
